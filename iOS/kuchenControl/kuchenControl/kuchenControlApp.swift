7ff7fcce256b0da23f313452eccd2b917daa1e1c
Fernando <fsalinas628@gmail.com>
2023-02-06 20:50:56 -0600

    Inicio de proyecto iOS

---
+++ iOS/kuchenControl/kuchenControl/kuchenControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  kuchenControlApp.swift
+//  kuchenControl
+//
+//  Created by Fernando Salinas on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct kuchenControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
